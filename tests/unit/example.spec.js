5a5b5015b106f6af5299b84055e0cec7667ce984
Alec Wertheimer <alec@wintreetz.com>
2022-07-01 10:32:27 -0400

    added more documentation

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -7,14 +7,17 @@ describe('Card.vue', () => {
     props: { cardName },
   });
 
+  //test to make sure the card component renders props
   it('renders props.cardName when passed', () => {
     expect(wrapper.text()).toMatch(cardName);
   });
 
+  // test to make sure card component renders a button
   it('has a button', () => {
     expect(wrapper.find('button').exists()).toBe(true);
   })
 
+  // test to make sure card compontent renders an image
   it('has an image', () => {
     expect(wrapper.find('img').exists()).toBe(true);
   })
